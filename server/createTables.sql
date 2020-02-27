2dc2b8388e0a3e72715d21db7a7946e1a4939bd4
cyNew <302974766@qq.com>
2020-02-27 14:13:16 +0800

    update sql definition

--- server/createTables.sql
+++ server/createTables.sql
@@ -7,21 +7,21 @@ CREATE TABLE `user` (
   `username` text NOT NULL,
   `password` text NOT NULL,
   `email` text NOT NULL,
-  `created` datetime NOT NULL,
   `isAuth` tinyint(4) NOT NULL DEFAULT '0',
-  `login` datetime DEFAULT NULL,
+  `created` text NOT NULL,
+  `login` text,
   PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Todo
-CREATE TABLE `todo` (s
+CREATE TABLE `todo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `todo` text NOT NULL,
   `completed` text NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
   `useridUserid` varchar(36) DEFAULT NULL,
+  `created` text NOT NULL,
+  `updated` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_7581941fdcddeff61bedd4214c9` (`useridUserid`),
   CONSTRAINT `FK_7581941fdcddeff61bedd4214c9` FOREIGN KEY (`useridUserid`) REFERENCES `user` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
