a2f09a75284b0f8d5ad7323e0b69227c258dbc89
cyNew <302974766@qq.com>
2020-02-27 13:00:30 +0800

    update todo controller

--- server/src/controller/todo.controller.ts
+++ server/src/controller/todo.controller.ts
@@ -8,107 +8,167 @@ enum Status {
 }
 
 const dbName: string = process.env.NODE_ENV === "production" ? "prod" : "test";
+
 class TodoController {
-  // find all todos
-  // URL: /api/v1/todo
+  // @desc    Get all Todos
+  // @route   GET /api/v1/todo
+  // @access  Public
   public static async getAllTodo(ctx: Context, next?: Next) {
     const todoRepo = getRepository(TodoTable, dbName);
-    const todos = await todoRepo.find({ userid: ctx.header.userid });
-    if (todos !== undefined && todos.length >= 1) {
+    try {
+      const todos: Array<TodoTable> = await todoRepo.find({
+        userid: ctx.header.userid
+      });
+
+      ctx.status = 200;
       ctx.body = {
+        success: true,
         data: todos
       };
-    } else {
-      ctx.status = 404;
-      return;
+    } catch (err) {
+      ctx.status = 500;
+      ctx.body = {
+        success: false,
+        msg: "Server Error"
+      };
     }
   }
 
-  // find a single todo
+  // @desc    Get single Todo
+  // @route   GET /api/v1/todo/:id
+  // @access  Public
   public static async getByIdTodo(ctx: Context, next?: Next) {
     const todoRepo = getRepository(TodoTable, dbName);
-    const todo = await todoRepo.findOne({ id: ctx.params.id });
-    if (!todo) {
-      ctx.status = 404;
-      return;
-    }
-    ctx.body = {
-      data: [todo]
-    };
-  }
 
-  public static async createTodo(ctx: Context, next?: Next) {
-    const todoRepo = getRepository(TodoTable, dbName);
-    const { todo } = ctx.request.body;
+    try {
+      const todo = await todoRepo.findOne({ id: ctx.params.id });
+      if (todo) {
 
-    if (!todo) {
-      ctx.status = 404;
-      ctx.body = {
-        msg: "todo is null"
-      };
-      return;
-    } else {
-      let newTodo = new TodoTable();
-      newTodo.todo = todo;
-      newTodo.completed = ctx.request.body.completed || Status.n;
-      newTodo.created = new Date();
-      newTodo.updated = new Date();
-      newTodo.userid = ctx.header.userid;
-      try {
-        await todoRepo.save(newTodo);
-        ctx.body = {
-          data: [newTodo]
-        };
-      } catch (error) {
-        ctx.status = 404;
-        console.error(error);
-        return;
-      }
-    }
-  }
-
-  public static async updateTodo(ctx: Context, next?: Next) {
-    const todoRepo = getRepository(TodoTable, dbName);
-    let todo = await todoRepo.findOne(ctx.params.id);
-
-    if (!todo) {
-      ctx.status = 404;
-      return;
-    } else {
-      todo.todo = ctx.request.body.todo || todo.todo;
-      todo.completed = ctx.request.body.completed || todo.completed;
-      todo.updated = new Date();
-      try {
-        await todoRepo.save(todo);
-        ctx.body = {
-          data: [todo]
-        };
-      } catch (error) {
-        ctx.status = 404;
-        console.error(error);
-        return;
-      }
-    }
-  }
-
-  public static async delTodo(ctx: Context, next?: Next) {
-    const todoRepo = getRepository(TodoTable, dbName);
-    const todo = await todoRepo.findOne(ctx.params.id);
-    if (!todo) {
-      ctx.status = 404;
-      return;
-    } else {
-      try {
-        await todoRepo.remove(todo);
         ctx.status = 200;
         ctx.body = {
+          success: true,
           data: [todo]
         };
-      } catch (error) {
+      } else {
         ctx.status = 404;
-        console.error(error);
-        return;
+        ctx.body = {
+          success: false,
+          msg: "Not Found Todo"
+        }
       }
+    } catch (err) {
+      ctx.status = 500;
+      ctx.body = {
+        success: false,
+        msg: "Server Error"
+      };
+    }
+  }
+
+  // @desc    Create a Todo
+  // @route   POST /api/v1/todo
+  // @access  Public
+  public static async createTodo(ctx: Context, next?: Next) {
+    const todoRepo = getRepository(TodoTable, dbName);
+    const { todo, completed } = ctx.request.body;
+    const { userid } = ctx.header;
+
+    const newTodo = new TodoTable();
+
+    try {
+      newTodo.todo = todo;
+      newTodo.completed = completed || Status.n;
+      newTodo.created = new Date().toLocaleString();
+      newTodo.updated = new Date().toLocaleString();
+      newTodo.userid = userid;
+
+      const data = await todoRepo.save(newTodo);
+
+      ctx.status = 201;
+      ctx.body = {
+        success: true,
+        data: [data]
+      };
+    } catch (err) {
+      console.log(err);
+      if (err.name === "QueryFailedError") {
+        ctx.status = 400;
+        ctx.body = {
+          success: false,
+          msg: err.message
+        };
+      } else {
+        ctx.status = 500;
+        ctx.body = {
+          success: true,
+          msg: "Server Error"
+        };
+      }
+    }
+  }
+
+  // @desc    Update a Todo
+  // @route   PUT /api/v1/todo/:id
+  // @access  Public
+  public static async updateTodo(ctx: Context, next?: Next) {
+    const todoRepo = getRepository(TodoTable, dbName);
+    try {
+      const todo = await todoRepo.findOne(ctx.params.id);
+      if (todo) {
+        todo.completed = Status.y || Status.n;
+        todo.updated = new Date().toLocaleString();
+        const data = await todoRepo.save(todo);
+
+        ctx.status = 200;
+        ctx.body = {
+          success: true,
+          data: [data]
+        };
+      } else {
+        ctx.status = 404;
+        ctx.body = {
+          success: false,
+          msg: "Not Found Todo"
+        };
+      }
+    } catch (err) {
+      ctx.status = 500;
+      ctx.body = {
+        success: false,
+        msg: err.message
+      };
+    }
+  }
+
+  // @desc    Delete a Todo
+  // @route   DELETE /api/v1/todo/:id
+  // @access  Public
+  public static async delTodo(ctx: Context, next?: Next) {
+    const todoRepo = getRepository(TodoTable, dbName);
+    const { id } = ctx.params;
+    try {
+      const todo = await todoRepo.findOne(id);
+      if (todo) {
+        const data = await todoRepo.remove(todo);
+
+        ctx.status = 200;
+        ctx.body = {
+          success: true,
+          data: [data]
+        };
+      } else {
+        ctx.status = 404;
+        ctx.body = {
+          success: false,
+          msg: "Not Found todo"
+        };
+      }
+    } catch (err) {
+      ctx.status = 500;
+      ctx.body = {
+        success: false,
+        msg: err.message
+      };
     }
   }
 }
